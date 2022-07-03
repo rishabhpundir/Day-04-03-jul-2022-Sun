47de5795b1fb2a5be3fce14a0ff2d42447e777b2
rishabhpundir <th3rishabh@gmail.com>
2022-07-03 14:52:13 +0530

    Day 04 03 Jul 2022 Sun

---
+++ day04.py
@@ -0,0 +1,73 @@
+# Task 1 
+# create a file 'studentData.txt'
+# open it and write 
+
+# Name : abc
+# Roll Number :  1
+# Class : 10
+# subject : English
+# Marks : 80
+
+
+
+
+# name = input("Name : ")
+# roll_no = input("Roll Number : ")
+# class_grade = input("Class : ")
+# subject = input("Subject : ")
+# marks = input("Marks : ")
+
+# with open("studentData.txt", mode='w') as file:
+#     file.write(f"Name : {name}\nRoll Number : {roll_no}\nClass : {class_grade}\nSubject : {subject}\nMarks : {marks}")
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Task 2 (New Program)
+
+# Take these Inputs from user
+
+# name
+# roll number
+# class
+# subject 1 Marks
+# subject 2 Marks
+# subject 3 Marks
+
+# example
+
+# name : abc
+# roll : number 1
+# subject 1 Marks : 10
+# subject 2 Marks : 20
+# subject 3 Marks : 30
+
+# create a file of entered name
+
+# example - abc.txt
+
+# Add Total marks and  write into file through python program
+
+# example 
+
+# name : abc
+# roll : number 1
+# subject 1 Marks : 10
+# subject 2 Marks : 20
+# subject 3 Marks : 30
+# Total : 60
+
+
+# name = input("Name : ")
+# roll_no = input("Roll Number : ")
+# subject1 = int(input("Subject 1 : "))
+# subject2 = int(input("Subject 2 : "))
+# subject3 = int(input("Subject 3 : "))
+# total_marks = subject1 + subject2 + subject3
+
+# with open(f"{name}.txt", mode='w') as file:
+#     file.write(f"Name : {name}\nRoll Number : {roll_no}\nSubject 1 Marks : {subject1}\nSubject 2 Marks : {subject2}\nSubject 3 Marks : {subject3}\nTotal Marks : {total_marks}/300")
+
+
+
